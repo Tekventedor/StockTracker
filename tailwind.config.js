c38d9f075964c71fe6c60999670afb341b571ace
Hugo Lewis Plant <hugolewisplant@Hugos-MacBook-Air-2.local>
2025-06-13 17:48:26 +0200

    Add old version styling support and restore glossary section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./layouts/**/*.html",
     "./content/**/*.{html,md}",
     "./themes/**/layouts/**/*.html",
-    "./assets/css/**/*.css"
+    "./assets/css/**/*.css",
+    "./old_version/Stocks/layouts/**/*.html",
+    "./old_version/Stocks/content/**/*.{html,md}"
   ],
   theme: {
     extend: {},
@@ -12,4 +14,20 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
   ],
+  prefix: {
+    'old-': 'old-', // Prefix for old version classes
+  },
+  safelist: [
+    // Add any classes that might be dynamically generated
+    'old-bg-blue-500',
+    'old-text-white',
+    'old-transition-all',
+    'old-duration-200',
+    'old-ease-in-out',
+    'old-hover:shadow-lg',
+    'old-hover:shadow-md',
+    'old-hover:bg-gray-50',
+    'old-hover:bg-blue-50',
+    'old-hover:bg-gray-200'
+  ]
 } 
