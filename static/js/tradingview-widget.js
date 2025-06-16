fbe32300eb30edcb29faaccec801e2ccf4c432e1
Hugo Lewis Plant <hugolewisplant@Hugos-MacBook-Air-2.local>
2025-06-16 14:57:21 +0200

    Move TradingView widget to static HTML file

---
+++ static/js/tradingview-widget.js
@@ -0,0 +1,12 @@
+{
+    "tabs": [
+        {
+            "title": "Indices",
+            "symbols": [
+                {"s": "FOREXCOM:SPXUSD", "d": "S&P 500"},
+                {"s": "FOREXCOM:NSXUSD", "d": "Nasdaq 100"},
+                {"s": "FOREXCOM:DJI", "d": "Dow 30"}
+            ]
+        }
+    ]
+} 
